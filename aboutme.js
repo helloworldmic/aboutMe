551271dc8ad4b0d1723217031ec1dd649203935c
helloworldmic <mychan2006@yahoo.com.hk>
2023-06-05 19:39:28 -0400

    Initial commit

---
+++ aboutme.js
@@ -0,0 +1,9 @@
+window.onscroll = function() {
+    if (window.scrollY > 40) {
+      document.getElementById("navbar").style.backgroundColor = "rbg(255,255,255)";
+    } else {
+      document.getElementById("navbar").style.backgroundColor = "rgba(10,10,10,0.6)";
+    }
+  }
+
+ 
